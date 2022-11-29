1e1a7bbf71338648eb2185e7b53edf67863d2f30
joaolucas2012 <ribeirojoaolucas68@gmail.com>
2022-11-29 00:33:12 -0300

    :bulb: [ADD] implement cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const router = require("./router/router");
+const cors = require("cors");
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors());
 app.use(router);
 
 app.listen(8080, function (req, res) {
